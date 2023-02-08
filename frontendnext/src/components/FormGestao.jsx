1c26271f8951f714877fbb0e98a82e2a54f76888
Mateus T <mateustavarestv@hotmail.com>
2023-02-08 18:00:46 -0300

    parte de gestão  completa falta enviar arquivo

---
+++ frontendnext/src/components/FormGestao.jsx
@@ -0,0 +1,183 @@
+import styled from "styled-components";
+import axios from "axios";
+import React, { useEffect, useRef } from "react";
+import { toast } from "react-toastify";
+
+const FormContainer = styled.form`
+  display: block;
+  background-color: #fff;
+  padding: 20px;
+  box-shadow: 0px 0px 5px #ccc;
+  border-radius: 5px;
+`;
+
+const InputArea = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-grow: 1;
+  align-items: flex-start;
+  flex-wrap:wrap;
+  max-width:1400px ;
+`;
+
+const Input = styled.input`
+  width: 120px;
+  padding: 0 10px;
+  border: 1px solid #bbb;
+  border-radius: 5px;
+  height: 40px;
+`;
+const H3 = styled.h3`
+`;
+
+const Label = styled.label`
+padding-right:10px ;
+`;
+
+const Button = styled.button`
+  padding: 10px;
+  cursor: pointer;
+  border-radius: 5px;
+  border: none;
+  background-color: #2c73d2;
+  color: white;
+  height: 42px;
+  margin-right:10px;
+  margin-top:10px ;
+`;
+
+
+export const FormGestao = ({ onEdit, setOnEdit, getGestao }) => {
+    const ref = useRef();
+
+    const handleOnEdit = () => {
+        setOnEdit(null)
+        const user = ref.current;
+        user.id_gestao.value = "";
+        user.dt_inicio.value = "";
+        user.dt_fim.value = "";
+        user.atos.value = "";
+        user.estatuto.value = "";
+        user.cpf_sindico.value = "";
+        user.cpf_subsindico.value = "";
+    }
+
+    const formatar_data = (dataTempo) => {
+        let data = new Date(dataTempo);
+        let dataFormatada = (data.getFullYear() + "-" + ((data.getMonth() + 1)) + "-" + (data.getDate()));
+        return dataFormatada;
+    }
+
+    useEffect(() => {
+        if (onEdit) {
+            const user = ref.current;
+            user.id_gestao.value = onEdit.ID_GESTAO;
+            user.dt_inicio.value = formatar_data(onEdit.DT_INICIO);
+            user.dt_fim.value = formatar_data(onEdit.DT_FIM);
+            user.atos.value = onEdit.ATOS;
+            user.estatuto.value = onEdit.ESTATUTO;
+            user.cpf_sindico.value = onEdit.CPF_SINDICO;
+            user.cpf_subsindico.value = onEdit.CPF_SUBSINDICO;
+        }
+    }, [onEdit]);
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        const user = ref.current;
+
+        if (
+            !user.id_gestao.value ||
+            !user.dt_inicio.value ||
+            !user.dt_fim.value ||
+            !user.atos.value ||
+            !user.estatuto.value ||
+            !user.cpf_sindico.value ||
+            !user.cpf_subsindico.value
+        ) {
+            return toast.warn("Preencha todos os campos!");
+        }
+        //    console.log(onEdit)
+        if (onEdit) {
+            await axios
+                .put("http://localhost:8800/gestao/" + onEdit.CPF, {
+                    id_gestao: user.id_gestao.value,
+                    dt_inicio: formatar_data(user.dt_inicio.value),
+                    dt_fim: formatar_data(user.dt_fim.value),
+                    atos: user.atos.value,
+                    estatuto: user.estatuto.value,
+                    cpf_sindico: user.cpf_sindico.value,
+                    cpf_subsindico: user.cpf_subsindico.value,
+                })
+                .then(({ data }) => toast.success(data))
+                .catch(({ data }) => toast.error(data));
+        } else {
+            await axios
+                .post("http://localhost:8800/gestao", {
+                    id_gestao: user.id_gestao.value,
+                    dt_inicio: formatar_data(user.dt_inicio.value),
+                    dt_fim: formatar_data(user.dt_fim.value),
+                    atos: user.atos.value,
+                    estatuto: user.estatuto.value,
+                    cpf_sindico: user.cpf_sindico.value,
+                    cpf_subsindico: user.cpf_subsindico.value,
+                })
+                .then(({ data }) => toast.success(data))
+                .catch(({ data }) => toast.error(data));
+        }
+
+        user.id_gestao.value = "";
+        user.dt_inicio.value = "";
+        user.dt_fim.value = "";
+        user.atos.value = "";
+        user.estatuto.value = "";
+        user.cpf_sindico.value = "";
+        user.cpf_subsindico.value = "";
+
+        setOnEdit(null);
+        getGestao();
+    };
+
+    return (
+        <FormContainer ref={ref} onSubmit={handleSubmit} enctype="multipart/form-data">
+            <H3>Gestão</H3>
+            <InputArea>
+                {onEdit ?
+                    <>
+                        <Label>Id - Gestão</Label>
+                        <Input name="id_gestao" disabled />
+                    </>
+                    :
+                    <>
+                        <Label>Id - Gestão</Label>
+                        <Input name="id_gestao" />
+                    </>
+                }
+                <Label>Data de Inicio</Label>
+                <Input name="dt_inicio" />
+                <Label>Data de Fim</Label>
+                <Input name="dt_fim" />
+                <Label>Atos</Label>
+                <Input name="atos" />
+                <Label>Estatuto</Label>
+                <input type="file" name="estatuto" />
+                <Label>CPF - Sindico</Label>
+                <Input name="cpf_sindico" />
+                <Label>CPF - Subsindico</Label>
+                <Input name="cpf_subsindico" />
+            </InputArea>
+            <Button type="submit">SALVAR</Button>
+            {onEdit ?
+                <>
+                    <Button onClick={() => handleOnEdit()}>Cancelar Edição</Button>
+                </>
+                :
+                <>
+                    {""}
+                </>
+            }
+        </FormContainer>
+
+    );
+
+};
--- frontendnext/src/pages/gestao.js
+++ frontendnext/src/pages/gestao.js
@@ -1,7 +1,62 @@
-export default function proprietarios() {
+import styled from "styled-components";
+import { useEffect, useState } from "react";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import axios from "axios";
+import { FormProprietario } from "@/components/FormProprietarios";
+import { GridProprietarios } from "@/components/GridProprietarios";
+import { FormGestao } from "@/components/FormGestao";
+import { GridGestao } from "@/components/GridGestao";
+
+
+
+const Container = styled.div`
+  width: 100%;
+  max-width: 1500px;
+  margin-top: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 10px;
+`;
+const Button = styled.button`
+  padding: 10px;
+  cursor: pointer;
+  border-radius: 5px;
+  border: none;
+  background-color: #2c73d2;
+  color: white;
+  height: 42px;
+`;
+
+const Title = styled.h2``;
+
+export default function gestao() {
+    const [gestao, setGestao] = useState([]);
+    const [onEdit, setOnEdit] = useState(null);
+
+    const getGestao = async () => {
+        try {
+            const res = await axios.get("http://localhost:8800/gestao");
+            setGestao(res.data.sort((a, b) => (a.nome > b.nome ? 1 : -1)));
+        } catch (error) {
+            toast.error(error);
+        }
+    };
+
+    useEffect(() => {
+        getGestao();
+    }, [setGestao]);
     return (
         <>
-            <h1>Gestão</h1>
+            <Container>
+                <Title>Gestão</Title>
+                <FormGestao setOnEdit={setOnEdit} onEdit={onEdit} getGestao={getGestao} />
+                <GridGestao gestao={gestao} setGestao={setGestao} setOnEdit={setOnEdit} />
+                <Button><a href="/">Voltar</a></Button>
+            </Container>
+
+            <ToastContainer autoClose={3000} position={toast.POSITION.BOTTOM_LEFT} />
         </>
     )
 }
